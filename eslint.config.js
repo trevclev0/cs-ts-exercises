bcd7ecf78491bed9b7b6f826c33fb6fdbde43128
Trevor Durán <trevor.m.duran@gmail.com>
2025-03-01 22:17:40 -0600

    Configured eslint for browser

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,9 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  { ignores: [".yarn/", ".pnp.cjs", ".pnp.mjs", "coverage/"] },
 ];
