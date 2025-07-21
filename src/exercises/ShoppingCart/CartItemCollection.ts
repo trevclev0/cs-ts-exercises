45bcfaf4128152fb69a2cd0a804f775faac25d07
Trevor Durán <trevclev0@gmail.com>
2025-07-21 17:14:42 -0500

    docs: add documentation for CartItemCollection methods (#2)
    * docs: add documentation for CartItemCollection methods
    
    * fix: moved the GitHub Actions to the correct location
    
    * fix: run unit tests only once when with PR
    
    ---------
    
    Co-authored-by: Trevor Durán <trevor.m.duran@gmail.com>

--- src/exercises/ShoppingCart/CartItemCollection.ts
+++ src/exercises/ShoppingCart/CartItemCollection.ts
@@ -1,4 +1,10 @@
 export class CartItem {
+    /**
+     * Constructor for CartItem. This does not do anything, all values
+     * are assigned through the public properties.
+     * @param productId The id of the product
+     * @param quantity The quantity of the product in the cart
+     */
     constructor(
         public productId: string,
         public quantity: number,
@@ -10,6 +16,12 @@ export class CartItem {
 export class CartItemCollection {
     private items: Map<string, CartItem> = new Map();
 
+    /**
+     * Adds a product to the collection or updates the quantity if it already exists.
+     * @param productId - The identifier of the product to add.
+     * @param quantity - The quantity of the product to add.
+     */
+
     add(productId: string, quantity: number): void {
         const currentItem = this.items.get(productId);
         if (currentItem) {
@@ -19,6 +31,12 @@ export class CartItemCollection {
         }
     }
 
+    /**
+     * Updates the quantity of the product in the cart. If the quantity is less than
+     * or equal to 0, the product is removed from the cart.
+     * @param productId - The identifier of the product to update.
+     * @param quantity - The new quantity of the product.
+     */
     update(productId: string, quantity: number): void {
         if (quantity <= 0) {
             this.remove(productId);
@@ -27,10 +45,18 @@ export class CartItemCollection {
         }
     }
 
+    /**
+     * Removes a product from the collection.
+     * @param productId - The identifier of the product to remove.
+     */
     remove(productId: string): void {
         this.items.delete(productId);
     }
 
+    /**
+     * Gets all items in the cart.
+     * @returns An array of CartItem
+     */
     getAll(): CartItem[] {
         return Array.from(this.items.values());
     }
