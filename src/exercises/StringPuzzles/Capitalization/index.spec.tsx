fb83d8f36a8a85a03cf2ee3851170a329dc70dae
Trevor Durán <trevor.m.duran@gmail.com>
2025-08-29 18:21:43 -0500

    ✨ feat: add function for capitalizing the first letter, but lowercasing the other letters of word

---
+++ src/exercises/StringPuzzles/Capitalization/index.spec.tsx
@@ -0,0 +1,37 @@
+import { describe, expect, it } from "@jest/globals";
+
+import { TestTableType } from "../../../types/TestTypes";
+
+import solution from ".";
+
+type CapitalizeWordsTableT = TestTableType<string, string>;
+
+const capitalizeWordsTests: CapitalizeWordsTableT[] = [
+    { input: "SoME rAndoM _TeXT", expected: "Some Random _text" },
+    { input: "hello world", expected: "Hello World" },
+    { input: "HELLO WORLD", expected: "Hello World" },
+    { input: "123 hello", expected: "123 Hello" },
+    { input: "_underscore", expected: "_underscore" },
+    {
+        input: "first second third fourth fifth sixth seventh eights ninth tenth",
+        expected:
+            "First Second Third Fourth Fifth Sixth Seventh Eights Ninth Tenth",
+    },
+    { input: "single", expected: "Single" },
+    {
+        input: "Hello neat pythonistas_123",
+        expected: "Hello Neat Pythonistas_123",
+    },
+    { input: "SoME rAndoM _TeXT", expected: "Some Random _text" },
+    { input: "CAPS lock IS on", expected: "Caps Lock Is On" },
+    { input: "mIxEd CaSe sample", expected: "Mixed Case Sample" },
+];
+
+describe("Capitalize words exercise", () => {
+    it.each(capitalizeWordsTests)(
+        "Capitalized words $#",
+        ({ input, expected }) => {
+            expect(solution(input)).toEqual(expected);
+        },
+    );
+});
