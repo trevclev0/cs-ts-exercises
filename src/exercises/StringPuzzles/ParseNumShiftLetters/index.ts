52db8fb74ce9ccaabd7caf353abcfc6583a3fd84
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-15 09:44:27 -0500

    ♻️ refactor(frankennumstring): simplify the solution by moving the replacement function inline
    Because the replacement function turned out to be much easier than originally planned, moving it
    inline with the .replace() helps readability

--- src/exercises/StringPuzzles/ParseNumShiftLetters/index.ts
+++ src/exercises/StringPuzzles/ParseNumShiftLetters/index.ts
@@ -5,28 +5,19 @@
 const numStrPairRegex = /(?<![-.\d])(\d+)[\s\p{P}]+([a-zA-Z])/gu;
 
 /**
- * Identifies the head of the FrankenNumString.
- * The head consists of the number and the first letter, stripped of all characters between them
- * and then swapped in place
+ * Convert string to contain FrankenNumStrings. Utilized the RegEx to dentify the head of the FrankenNumString.
+ * The head consists of the number and the first letter, stripped of all whitespace and puncutation between them.
+ * The head of the FrankenNumString is replaced within the appropriate place in the input string.
  *
- * @param match The matched string from the Regex
- * @returns The head of the FrankenNumString
- */
-const replaceFunc = (_: string, num: string, char: string): string => {
-    return char + num;
-};
-
-/**
- * Convert string to contain FrankenNumStrings
  * FrankenNumStrings are strings where:
  * - Each positive integer is identified
  * - The letter following the number is identified
  * - The number is placed immediately after the letter
  * - All characters between the number and the letter are removed
  *
- * @param input The string to convert
+ * @param input The string to convert, swapping out the FrankenNumStrings as needed
  * @returns The converted string
  */
 export default function solution(input: string): string {
-    return input.replace(numStrPairRegex, replaceFunc);
+    return input.replace(numStrPairRegex, (_, num, char) => char + num);
 }
