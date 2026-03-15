ac11262ac32a2f85b1fa13c9b67b3190cfff4922
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-15 15:17:25 -0500

    ✨ feat(types): add a Type for creating tests with a description (closes #20)
    closes #20

--- src/types/TestTypes.ts
+++ src/types/TestTypes.ts
@@ -2,3 +2,8 @@ export interface TestTableType<TInput, TExpected> {
     input: TInput;
     expected: TExpected;
 }
+
+export interface TestTableWithDescriptionType<TInput, TExpected>
+    extends TestTableType<TInput, TExpected> {
+    description: string;
+}
