400236830ebcdb1d56131a5dd0bc09b0af1dc1e6
Trevor Durán <trevclev0@gmail.com>
2026-03-18 18:07:51 +0000

    :sparkles: feat: implement simple solution

--- src/exercises/StringOpsTypeConversions/TimeParser/index.spec.ts
+++ src/exercises/StringOpsTypeConversions/TimeParser/index.spec.ts
@@ -13,14 +13,14 @@ type TimeParseTestTable = TestTableType<SolutionParams, string>;
 
 const timeParseTests: TimeParseTestTable[] = [
     {
-        testName: "parse time",
+        testName: "adds 123 seconds to 05:10:30",
         input: { start: "05:10:30", ffSecs: 123 },
         expected: "05:12:33",
     },
 ];
 
 describe("Time Parser", () => {
-    it.todo.each(timeParseTests)("$testName", ({ input, expected }) => {
+    it.each(timeParseTests)("$testName", ({ input, expected }) => {
         const { start, ffSecs } = input;
         expect(solution(start, ffSecs)).toEqual(expected);
     });
--- src/exercises/StringOpsTypeConversions/TimeParser/index.ts
+++ src/exercises/StringOpsTypeConversions/TimeParser/index.ts
@@ -1,3 +1,44 @@
+import { TimeSplit, TimeSplitStr } from "../../../types/Time";
+
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = 60 * SECONDS_PER_MINUTE;
+
+function getTimeSplit(inputTime: string): TimeSplit {
+    const [hoursString, minutesString, secondsString] = inputTime.split(":");
+    const hours = Number.parseInt(hoursString, 10);
+    const minutes = Number.parseInt(minutesString, 10);
+    const seconds = Number.parseInt(secondsString, 10);
+
+    return { hours, minutes, seconds };
+}
+
+function getTimeToAdd(inputSecs: number): TimeSplit {
+    const hr = Math.floor(inputSecs / SECONDS_PER_HOUR);
+    const min = Math.floor(inputSecs / SECONDS_PER_MINUTE);
+    const sec = inputSecs - hr * SECONDS_PER_HOUR - min * SECONDS_PER_MINUTE;
+
+    return { hours: hr, minutes: min, seconds: sec };
+}
+function toTimeStr(input: number): string {
+    return String(input).padStart(2, "0");
+}
+
+function toTimeSplitString(hr: number, min: number, sec: number): TimeSplitStr {
+    const hours = toTimeStr(hr);
+    const minutes = toTimeStr(min);
+    const seconds = toTimeStr(sec);
+
+    return { hours, minutes, seconds };
+}
+
 export default function solution(start: string, ffSecs: number): string {
-    return "TODO";
+    const { hours: hrs, minutes: mins, seconds: secs } = getTimeSplit(start);
+    const { hours: hrA, minutes: minA, seconds: secA } = getTimeToAdd(ffSecs);
+    const { hours, minutes, seconds } = toTimeSplitString(
+        hrs + hrA,
+        mins + minA,
+        secs + secA,
+    );
+
+    return `${hours}:${minutes}:${seconds}`;
 }
