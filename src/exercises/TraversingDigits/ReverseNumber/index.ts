c6ae4cde34c6096cf9f2ed1c5301fa2eee6b6fe4
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-17 21:47:39 -0500

    :recycle: refactor: optimize ReverseNumber

--- src/exercises/TraversingDigits/ReverseNumber/index.ts
+++ src/exercises/TraversingDigits/ReverseNumber/index.ts
@@ -2,10 +2,8 @@ export default function solution(number: number): number {
     let result = 0;
     let num = number;
     do {
-        const digit: number = num % 10;
-        result += digit;
+        result = result * 10 + (num % 10);
         num = Math.floor(num / 10);
-        result *= 10;
     } while (num > 0);
-    return result / 10;
+    return result;
 }
