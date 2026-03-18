0c17c95076f619f08da2c2e40d4af6dd01208b4a
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-17 21:50:26 -0500

    :recycle: refactor: optimize ReverseTuplets with a Set

--- src/exercises/TraversingDigits/ReverseTuplets/index.ts
+++ src/exercises/TraversingDigits/ReverseTuplets/index.ts
@@ -2,9 +2,11 @@ import reverseNumber from "../ReverseNumber";
 
 export function solution(numbers: number[]): [number, number][] {
     const result: [number, number][] = [];
+    const numSet = new Set(numbers);
+
     for (const num of numbers) {
         const reverseNum = reverseNumber(num);
-        if (numbers.includes(reverseNum)) {
+        if (numSet.has(reverseNum)) {
             result.push([num, reverseNum]);
         }
     }
