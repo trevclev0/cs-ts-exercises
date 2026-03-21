78db5d7d67e66a446c2ebec9bbb6a14437738bcf
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-20 20:15:41 -0500

    :sparkles: feat: implement time period calculations challenge

--- src/exercises/StringOpsTypeConversions/TimePeriod/index.ts
+++ src/exercises/StringOpsTypeConversions/TimePeriod/index.ts
@@ -1,3 +1,50 @@
+import {
+    getTimeSplit,
+    MINS_PER_HR,
+    timeSplitToSecs,
+} from "../../../utils/time";
+
+// Valid time period is one which has:
+// - Only digits, colons, and a hyphen separating two times
+// - Both times must be in the format HH:MM:SS
+const TIME_PERIOD_REGEX =
+    /^(?<startTime>(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d) - (?<endTime>(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d)$/;
+
+/**
+ * Takes a time period in the format HH:MM:SS - HH:MM:SS and returns the duration of the time period in minutes.
+ * Throws an error if the input is invalid or the start time is after the end time.
+ * @param {string} input - The time period in the format HH:MM:SS - HH:MM:SS
+ * @returns {number} - The duration of the time period in minutes
+ */
 export default function solution(input: string): number {
-    return NaN;
+    const match = input.match(TIME_PERIOD_REGEX);
+    if (!match?.groups) {
+        throw new Error("Invalid input");
+    }
+
+    const { startTime, endTime } = match.groups;
+    const startTimeSplit = getTimeSplit(startTime);
+    const { hours: sHrs, minutes: startMins, seconds: sSecs } = startTimeSplit;
+    const startSecs = timeSplitToSecs({
+        hours: sHrs,
+        minutes: startMins,
+        seconds: sSecs,
+    });
+    const endTimeSplit = getTimeSplit(endTime);
+    const { hours: eHrs, minutes: endMins, seconds: eSecs } = endTimeSplit;
+    const endSecs = timeSplitToSecs({
+        hours: eHrs,
+        minutes: endMins,
+        seconds: eSecs,
+    });
+
+    if (startSecs > endSecs) {
+        throw new Error("Start time must be before end time");
+    }
+
+    const diffSplitMins = endMins - startMins;
+    const diffSplitHrs = eHrs - sHrs;
+    const timePeriodMins = diffSplitMins + diffSplitHrs * MINS_PER_HR;
+
+    return timePeriodMins;
 }
