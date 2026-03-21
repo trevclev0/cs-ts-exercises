3c77b69160274f0aa57c03666808639bb2f2fc90
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-20 20:15:41 -0500

    :sparkles: feat: implement time period calculations challenge

--- src/exercises/StringOpsTypeConversions/TimePeriod/index.spec.ts
+++ src/exercises/StringOpsTypeConversions/TimePeriod/index.spec.ts
@@ -4,9 +4,71 @@ import { ErrorTableType, TestTableType } from "../../../types/TestTypes";
 
 import solution from ".";
 
-const timePeriodTests: TestTableType<string, string>[] = [];
+const timePeriodTests: TestTableType<string, number>[] = [
+    {
+        testName: "start: 12:15:00, end: 12:16:59, duration: 1min 59s",
+        input: "12:15:00 - 12:16:59",
+        expected: 1,
+    },
+    {
+        testName: "start: 12:14:59, end: 12:15:00, duration: 1s",
+        input: "12:14:59 - 12:15:00",
+        expected: 1,
+    },
+    {
+        testName: "start: 00:00:00, end: 00:00:01, duration: 1s",
+        input: "00:00:00 - 00:00:01",
+        expected: 0,
+    },
+    {
+        testName: "start: 00:00:00, end: 00:01:00, duration: 1min",
+        input: "00:00:00 - 00:01:00",
+        expected: 1,
+    },
+    {
+        testName: "start: 00:59:59, end: 01:00:00, duration: 1s",
+        input: "00:59:59 - 01:00:00",
+        expected: 1,
+    },
+    {
+        testName: "start: 00:00:00, end: 23:59:59, duration: 23hr 59min 59s",
+        input: "00:00:00 - 23:59:59",
+        expected: 1439,
+    },
+    {
+        testName: "start: 01:05:05, end: 16:30:50, duration: 15hr 25min 45s",
+        input: "01:05:05 - 16:30:50",
+        expected: 925,
+    },
+    {
+        testName: "start: 12:15:30, end: 14:00:00, duration: 1hr 44min 30s",
+        input: "12:15:30 - 14:00:00",
+        expected: 105,
+    },
+    {
+        testName: "start: 02:45:20, end: 06:37:35, duration: 3hr 52min 15s",
+        input: "02:45:20 - 06:37:35",
+        expected: 232,
+    },
+];
 
-const timePeriodErrorTests: ErrorTableType<string>[] = [];
+const timePeriodErrorTests: ErrorTableType<string>[] = [
+    {
+        testName: "empty time period",
+        input: "",
+        errorMsg: "Invalid input",
+    },
+    {
+        testName: "no hyphen between times",
+        input: "00:00:00 23:59:59",
+        errorMsg: "Invalid input",
+    },
+    {
+        testName: "start time greater than end time",
+        input: "03:00:00 - 01:00:00",
+        errorMsg: "Start time must be before end time",
+    },
+];
 
 describe("Time Period Parser", () => {
     describe("Successful parsing", () => {
