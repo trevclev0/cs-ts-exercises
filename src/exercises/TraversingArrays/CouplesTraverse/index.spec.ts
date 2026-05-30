9330f2f30c96073504c9d386f2609455ef853805
Trevor Durán <trevor.m.duran@gmail.com>
2026-05-30 18:35:11 -0500

    :white_check_mark: test: Add test cases for edge scenarios

--- src/exercises/TraversingArrays/CouplesTraverse/index.spec.ts
+++ src/exercises/TraversingArrays/CouplesTraverse/index.spec.ts
@@ -8,6 +8,11 @@ type TraversedCouplesTestTable = TestTableType<number[], number[]>;
 
 const traversedCouplesTests: TraversedCouplesTestTable[] = [
   {
+    testName: "returns an empty arrya when provided an empty array",
+    input: [],
+    expected: [],
+  },
+  {
     testName: "odd length five",
     input: [1, 2, 3, 4, 5],
     expected: [3, 1, 2, 4, 5],
@@ -26,6 +31,21 @@ const traversedCouplesTests: TraversedCouplesTestTable[] = [
     testName: "odd length eleven",
     input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
     expected: [6, 4, 5, 7, 8, 2, 3, 9, 10, 1, 11],
+  },
+  {
+    testName: "traverses an even-cubic-length array correctly",
+    input: [10, 20, 30, 40],
+    expected: [30, 10, 20, 40],
+  },
+  {
+    testName: "traverses an even-length array correctly",
+    input: [1, 2, 3, 4, 5, 6],
+    expected: [4, 2, 3, 5, 6, 1],
+  },
+  {
+    testName: "handles an array with a single-digit single element correctly",
+    input: [1],
+    expected: [1],
   },
   { testName: "single element", input: [100], expected: [100] },
 ];
